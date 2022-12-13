6413ba7d21680ff31f8bad2b8ccbf84548295e20
hsymicm <ichorvfx@gmail.com>
2022-12-13 17:05:21 +0700

    Add Footer

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,12 +1,14 @@
 import '@/assets/styles/Style.css'
 import NavBar from '@/components/Layout'
 import Router from '@/routes/Router'
+import Footer from '@/components/Footer'
 
 export default function App() {
   return (
     <>
       <NavBar />
       <Router />
+      <Footer />
     </>
   )
 }
