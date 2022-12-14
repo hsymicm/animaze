43b732930dff542a8a8eec752e6972b0d92e7e4d
hsymicm <ichorvfx@gmail.com>
2022-12-14 17:54:49 +0700

    Minor Modification

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
+import ScrollToTop from '../modules/ScrollToTop'
 import Home from '@/pages/Home'
 import About from '@/pages/About'
 import Watchlist from '@/pages/Watchlist'
 
 export default function Router() {
     return (
+        <>
+        <ScrollToTop />
         <Routes>
             <Route exact path="/" element={ <Home /> } />
             <Route path="/about" element={ <About /> } />
             <Route path="/watchlist" element={ <Watchlist /> } />
         </Routes>
+        </>
     )
 }
