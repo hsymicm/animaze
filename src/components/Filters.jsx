101b05a8cf8cb4a5a007f10da15294cff582fc63
hsymicm <ichorvfx@gmail.com>
2022-12-17 19:30:13 +0700

    Update

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -5,7 +5,7 @@ export default function Filters({text, filter}) {
     return (
         <>
         <h4 className='title'>{text}</h4>
-        {filter.map((item) => <div className='filter'><ComboBox className="filter" filter={item.filter} options={item.option}/></div>)}
+        {filter.map((item, key) => <div key={key} className='filter'><ComboBox className="filter" filter={item.filter} options={item.option}/></div>)}
         </>
     )
 }
--- src/components/Lists.jsx
+++ src/components/Lists.jsx
@@ -5,12 +5,12 @@ export default function Lists({options, onOptionChange}) {
         <>
         <h4 className='title'>Lists</h4>
         <ul className='list'>
-            {options.map((option, key) => (
+            {Object.keys(options).map((option, key) => (
                 <li 
                 key={key} 
-                className={option.isSelected ? 'active' : ''} 
-                onClick={() => onOptionChange(key)}>
-                    {option.option}
+                className={options[option].isSelected ? 'active' : ''} 
+                onClick={() => onOptionChange(option)}>
+                    {options[option].option}
                 </li>
             ))}
         </ul>
