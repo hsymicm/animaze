e9828d30c67ae10884e170a221859e5fe901e602
hsymicm <ichorvfx@gmail.com>
2022-12-18 19:52:34 +0700

    Add New Data

---
+++ src/assets/data/Format.js
@@ -0,0 +1,11 @@
+const Format = [
+    'TV',
+    'TV Short',
+    'Movie',
+    'Special',
+    'OVA',
+    'ONA',
+    'Music'
+]
+
+export default Format
---
+++ src/assets/data/Genre.js
@@ -0,0 +1,22 @@
+const Genre = [
+    "Action",
+    "Adventure",
+    "Comedy",
+    "Drama",
+    "Ecchi",
+    "Fantasy",
+    "Horror",
+    "Mahou Shoujo",
+    "Mecha",
+    "Music",
+    "Mystery",
+    "Psychological",
+    "Romance",
+    "Sci-Fi",
+    "Slice of Life",
+    "Sports",
+    "Supernatural",
+    "Thriller",
+];
+
+export default Genre;
