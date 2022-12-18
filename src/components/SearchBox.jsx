75810cd0cd64364078821c00f1190af76fc11599
hsymicm <ichorvfx@gmail.com>
2022-12-18 19:53:20 +0700

    Searchbox Logic Rewrite

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -1,12 +1,21 @@
 import search from '@/assets/svgs/magnifying-glass-solid.svg'
 import '@/assets/styles/SearchBox.css'
 import '@/assets/styles/Style.css'
+import { useState } from 'react'
 
-export default function SearchBox({text}) {
+
+export default function SearchBox({searchShows}) {
+    const [value, setValue] = useState('')
+
+    const handleChange = (e) => {
+        const val = e.target.value
+        setValue(val)
+        searchShows(val)
+    }
     return (
         <div className='search-box'>
             <img src={search} />
-            <input type="text" placeholder={text} className="search-box" />
+            <input value={value} onChange={handleChange} type="text" placeholder="Filter" className="search-box" />
         </div>
     )
 }
