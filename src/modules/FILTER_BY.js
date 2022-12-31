a502f49b30b2a806408973ab4fa7e760aab24c48
hsymicm <ichorvfx@gmail.com>
2022-12-31 13:17:35 +0700

    Remove docs

--- src/modules/FILTER_BY.js
+++ src/modules/FILTER_BY.js
@@ -1,21 +1,9 @@
-/**
- * Filter Object by Status
- * @param {Object} obj - Anime data
- * @param {string} curStatus - Current status filter
- * @returns {Object} Filtered object by status
- */
 const filterByStatus = (obj, curStatus) => {
     if(curStatus === 'All') return obj
     let temp = {}
     return {...temp, [curStatus] : obj[curStatus]}
 }
 
-/**
- * Filter Object by Search Queries
- * @param {Object} obj - Anime data 
- * @param {string} query - Search filter queries
- * @returns {Object} Filtered object by search queries
- */
 const filterBySearch = (obj, query) => {
     if(!query) return obj
     let temp = {}
