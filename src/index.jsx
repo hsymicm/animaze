5f5fdd954cfb20ac016bf4a8c124ea61f33204bd
hsymicm <ichorvfx@gmail.com>
2022-12-31 13:15:08 +0700

    Add Basename

--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 import App from '@/pages/App'
 
 createRoot(document.getElementById('root')).render(
-	<BrowserRouter>
+	<BrowserRouter basename={import.meta.env.BASE_URL} >
 		<App />
 	</BrowserRouter>
 )
