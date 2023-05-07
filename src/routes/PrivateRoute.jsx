0d3272bd705275fd84327e6e4c3d39be169f2a9c
hsymicm <ichorvfx@gmail.com>
2023-05-08 01:23:11 +0700

    Add Toast Message

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -1,8 +1,14 @@
 import { Navigate } from 'react-router-dom';
+import { useEffect } from 'react';
+import toast from 'react-hot-toast';
 import { useAuth } from '@/contexts/AuthContext';
 
 function PrivateRoute({ element }) {
   const { currentUser } = useAuth();
+
+  useEffect(() => {
+    if (!currentUser) toast.error('You need to be signed in');
+  }, []);
 
   return currentUser ? (
     element
