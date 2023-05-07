13b4623f66aa41600daa728987000c13ca743847
hsymicm <ichorvfx@gmail.com>
2023-05-08 01:16:48 +0700

    Mobile Responsive Context

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,16 @@
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from '@/contexts/AuthContext';
+import { MobileProvider } from '@/contexts/MobileContext';
 import 'react-tooltip/dist/react-tooltip.css';
 import App from '@/pages/App';
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter basename={import.meta.env.BASE_URL}>
     <AuthProvider>
-      <App />
+      <MobileProvider>
+        <App />
+      </MobileProvider>
     </AuthProvider>
   </BrowserRouter>
 );
